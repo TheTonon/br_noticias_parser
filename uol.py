d259147bdd9ead251f416ffb4bd43c16cba43ef8
Vinicius Tonon <Tonon@MBP-de-Vinicius.lan>
2017-11-19 18:38:28 -0200

    Eu não entendia packages, mas não sei se entendo agora.

--- uol.py
+++ uol.py
@@ -4,15 +4,14 @@ import urllib.request
 import re
 from bs4 import BeautifulSoup
 
-class uol:
-    def parse(html):
-        sopa = BeautifulSoup(html, "lxml")
-        artigo = sopa.find("article")
-        div_texto = sopa.find("div", id_=re.compile('^texto'))
-        paragrafos = div_texto.find_all("p")
-        texto = []
-        for paragrafo in paragrafos:
-            texto.append("\n")
-            texto.append(paragrafo.get_text())
-        return ''.join(texto)
+def parse(html):
+    sopa = BeautifulSoup(html, "lxml")
+    artigo = sopa.find("article")
+    div_texto = sopa.find("div", id_=re.compile('^texto'))
+    paragrafos = div_texto.find_all("p")
+    texto = []
+    for paragrafo in paragrafos:
+        texto.append("\n")
+        texto.append(paragrafo.get_text())
+    return ''.join(texto)
 
