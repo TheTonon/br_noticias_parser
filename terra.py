d3ea276afefa1f676d484d15e021b84c542aa14a
Vinicius Tonon <viniciustonon@gmail.com>
2017-11-20 20:40:06 -0200

    Social Share não aparece mais no Terra

--- terra.py
+++ terra.py
@@ -10,12 +10,12 @@ class terra:
         artigo = re.sub('<[/em]*>','', str(sopa.find('div', class_=re.compile('^articleData'))))
         div_artigo = BeautifulSoup(artigo, 'lxml')
         for div in div_artigo.find_all('div', {'class':re.compile('related.*')}):
-            print("SEU REGEX TÁ CERTO")
+            div.decompose()
+        for div in div_artigo.find_all('div', {'class':re.compile('contentShare.*')}):
             div.decompose()
         paragrafos = div_artigo.find_all('p')
         texto = []
         for paragrafo in paragrafos:
-            #texto.append('\n')
             texto.append(paragrafo.get_text().replace('\n', '').replace('   ', ' ').replace('  ', ' '))
             texto.append('\n')
         return ''.join(texto)
