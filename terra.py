e93d00000e4388798ba4a2e75ab5fa62233705a7
Vinicius Tonon <viniciustonon@gmail.com>
2017-11-20 20:36:19 -0200

    Correção no Terra

--- terra.py
+++ terra.py
@@ -7,10 +7,15 @@ from bs4 import BeautifulSoup
 class terra:
     def parse(html):
         sopa = BeautifulSoup(html, "lxml")
-        div_artigo = sopa.find("div", class_=re.compile('^articleData'))
+        artigo = re.sub('<[/em]*>','', str(sopa.find('div', class_=re.compile('^articleData'))))
+        div_artigo = BeautifulSoup(artigo, 'lxml')
+        for div in div_artigo.find_all('div', {'class':re.compile('related.*')}):
+            print("SEU REGEX TÁ CERTO")
+            div.decompose()
         paragrafos = div_artigo.find_all('p')
         texto = []
         for paragrafo in paragrafos:
+            #texto.append('\n')
+            texto.append(paragrafo.get_text().replace('\n', '').replace('   ', ' ').replace('  ', ' '))
             texto.append('\n')
-            texto.append(paragrafo.get_text())
         return ''.join(texto)
