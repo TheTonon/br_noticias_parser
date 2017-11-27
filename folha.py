14310c459edbc27825a07e7f0fd3a39567687fb3
Vinicius Tonon <viniciustonon@gmail.com>
2017-11-27 17:29:41 -0200

    Definitive addition of Folha parser

---
+++ folha.py
@@ -0,0 +1,17 @@
+# -*-encoding:utf-8-*-
+
+import urllib.request
+import re
+from bs4 import BeautifulSoup
+
+class folha:
+    def parse(html):
+        soup = BeautifulSoup(html, "lxml")
+        article = soup.find('article',{'id':'news'})
+        content = article.find('div',{'class':'content'})
+        paragraphs = content.find_all('p')
+        final_text = []
+        for paragraph in paragraphs:
+            final_text.append("\n")
+            final_text.append(paragraph.get_text())
+        return ''.join(final_text)
