797231c1c99932fec123d80fad2e18e2708272e3
Vinicius Tonon <viniciustonon@gmail.com>
2017-11-30 15:14:36 -0200

    Refactored a bit of BrNP main class

--- BrNP.py
+++ BrNP.py
@@ -9,8 +9,21 @@ from r7 import r7
 from terra import terra
 from uol import uol
 from folha import folha
+from elpais import elpais
 
-lista_sites = ["folha.uol.com.br", "g1.globo.com", 'r7.com', "uol.com.br", "terra.com.br", "brasil247.com.br"]
+lista_sites = ["folha.uol.com.br",
+               "g1.globo.com",
+               "r7.com",
+               "uol.com.br",
+               "terra.com.br",
+               "brasil.elpais.com"]
+
+parsers = [folha.parse,
+           g1.parse,
+           r7.parse,
+           uol.parse,
+           terra.parse,
+           elpais.parse]
 
 def is_url(url):
     if url.startswith("http://") or url.startswith("https://"):
@@ -23,31 +36,15 @@ def is_url(url):
         return False
 
 def get_noticia(url):
-    contador_site_atual = 0
+    site_counter = 0
+    html = get_html(url)
     for site in lista_sites:
-        print ("{0}".format(site))
         busca = re.compile("{0}".format(site))
         if busca.search(url):
-            print ("fechou")
-            if contador_site_atual == 0:
-                html = get_html(url)
-                return (folha.parse(html))
-            if contador_site_atual == 1:
-                html = get_html(url)
-                return (g1.parse(html))
-            if contador_site_atual == 2:
-                html = get_html(url)
-                return r7.parse(html)
-            if contador_site_atual == 3:
-                html = get_html(url)
-                return uol.parse(html)
-            if contador_site_atual == 4:
-                html = get_html(url)
-                return terra.parse(html)
-            break
+            return parsers[site_counter](html)
         else:
-            contador_site_atual = contador_site_atual + 1
-            print ("Xabu")
+            site_counter = site_counter + 1
+    return "Ocorreu um erro. Provavelmente o site não é suportado"
 
 def get_html(url):
     if is_url(url):
@@ -55,26 +52,3 @@ def get_html(url):
         return r.text
     else:
         return "url invalida"
-
-def parse_g1(noticia_url):
-    noticia_html = get_html(noticia_url)
-    sopa = BeautifulSoup(noticia_html, "lxml")
-    texto_cru = sopa.find_all("p", class_=re.compile('^content-text__container'))
-    texto = ""
-    for txt in texto_cru:
-        print (txt.get_text())
-        texto.join("\n")
-        texto.join(txt.get_text())
-    return texto
-
-def parse_r7(noticia_url):
-    noticia_html = get_html(noticia_url)
-    sopa = BeautifulSoup(noticia_html, "lxml")
-    artigo = sopa.find("article")
-    texto_cru = artigo.find_all("p")
-    texto = ""
-    for txt in texto_cru:
-        print (txt.get_text())
-        texto.join("\n")
-        texto.join(txt.get_text())
-    return texto
